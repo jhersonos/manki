271da199c38f4a2b05bd98566e00dc88830b5f75
jherson onairam <jherson@MacBook-Pro-de-jherson.local>
2017-07-18 11:22:33 -0500

    ajax

--- js/partner.js
+++ js/partner.js
@@ -1,24 +1,45 @@
-var modal = document.getElementById('myModal');
+$(document).ready(function(){
+    var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
         modal.style.display = "none";
     }
-}
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    $('#send').click(function(){
+        var name = $('#nombre').val();
+        var empresa = $('#empresa').val();
+        var correo = $('#correo').val();
+        var numero = $('#numero').val();
+        var url = "";
+        if(name != "" && empresa != "" && correo !="" && numero !=""){
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: data,
+                success: success,
+                dataType: dataType
+            });
+        }else{
+            alert('Complete los datos correctamente');
+        }
+    })
+})
