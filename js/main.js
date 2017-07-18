5b17b91c33b4acddd6e81a18095b76c538f7dfed
jherson onairam <jherson@MacBook-Pro-de-jherson.local>
2017-07-18 12:08:37 -0500

    back

--- js/main.js
+++ js/main.js
@@ -5,8 +5,20 @@ $(document).ready(function(){
         },
         en:{
             body:"Hi!<br> I'm Manki, your Smart Travel Buddy on Facebook Messenger, I help you with information and recommendations for your travels for free. Message me to start planning your trip together."
-        }
+        },
+        back:[
+            "http://www.latinotravel.com.pe/wp-content/uploads/2015/08/Ruinas_majestuosas.jpg",
+            "http://www.latinotravel.com.pe/wp-content/uploads/2015/08/Ruinas_majestuosas.jpg",
+            "http://www.viajestdh.com.ar/wp-content/uploads/cuzco-plaza-armas.jpg",
+            "https://s.latamstatic.com/destinosudamerica/wp-content/uploads/2015/09/Calle-de-Cuzco-1024x683.jpg"
+        ]
     }
+    function random(){
+        var x = Math.floor((Math.random() * 3) + 1);
+        var str = contenido.back[x];
+        $('body .container .bg').css('background-image','url('+str+')');
+    }
+    random();
     $('.type').on('click',function(){
         var lang = $(this).attr('id') ;
         console.log(lang)
