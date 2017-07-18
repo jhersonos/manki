4b9ff50671ddc388440e1c0842b88aba1ee2a3d3
jherson onairam <jherson@MacBook-Pro-de-jherson.local>
2017-07-18 00:52:57 -0500

    all changes finish

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var contenido = {
         es:{
-            body:"Hola! <br> Soy Manki, tu Smart Travel Buddy en Facebook Messenger. Te ayudo con información y recomendaciones de manera gratuita para tus viajes. Escríbeme para comenzar a planear juntos tu viaje."
+            body:"¡Hola! <br> Soy Manki, tu Smart Travel Buddy en Facebook Messenger. Te ayudo con información y recomendaciones de manera gratuita para tus viajes. Escríbeme para comenzar a planear juntos tu viaje."
         },
         en:{
             body:"Hi!<br> I'm Manki, your Smart Travel Buddy on Facebook Messenger, I help you with information and recommendations for your travels for free. Message me to start planning your trip together."
